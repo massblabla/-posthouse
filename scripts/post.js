894bdced610cf337234ecc0102a62a0fbc310252
massblabla <alv4ro.m4ss@gmail.com>
2024-07-11 20:05:31 +0700

    hehe

--- scripts/post.js
+++ scripts/post.js
@@ -15,9 +15,29 @@ function getQuery() {
 
 async function loadPage() {
     const contentdiv = document.querySelector("#content");
+    const titlediv = document.querySelector("#title");
+    const authordiv = document.querySelector("#author");
+    const datetimediv = document.querySelector("#video");
+
+    const openSpan = "<span>";
+    const closeSpan = "</span>";
     /* Data Section */
     let data = await (textFetch(url + "data/" + getQuery() + ".json"));
     let json = JSON.parse(data);
+    let title_ = json.title;
+    let author_ = json.author;
+    let date_ = json.date;
+    let time_ = json.time;
+    let video_ = json.video;
+
+    let datetime_ = date_ + " " + time_ + " UTC";
+
+    let title = openSpan + title_ + closeSpan;
+    let author = openSpan + author_ + closeSpan;
+    let datetime = openSpan + datetime_ + closeSpan;
+    let video = "<video "
+
+    titlediv.innerHTML = title;
 
     /* Content Section */
     let content_ = await (textFetch(url + "content/" + getQuery() + ".txt"));
